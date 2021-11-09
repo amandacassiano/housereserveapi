0e2338235e0b7b0de794f03f45d74fe26d525410
Amanda Cassiano <amandacassi4@gmail.com>
2021-11-09 10:16:45 -0300

    correção

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ class App {
   constructor(){
     this.server = express();
 
-    mongoose.connect('',{
+    mongoose.connect(' ',{
       useNewUrlParser:true,
       useUnifiedTopology:true,
     });
